1a0995757285c3b964675fae479fbfe7be8df319
zain <zainmerchant00@yahoo.com>
2019-01-05 11:16:10 -0800

    added files for view pager

---
+++ app/src/main/java/com/android/wcf/fragments/child/MilestoneFragment.java
@@ -0,0 +1,21 @@
+package com.android.wcf.fragments.child;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+public class MilestoneFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+}
