b4f696933224ec9f94acb81dba309b522b9500e4
zain <zainmerchant00@yahoo.com>
2019-01-06 17:33:38 -0800

    added basic viewpager

--- app/src/main/java/com/android/wcf/activity/MilestoneDetailActivity.java
+++
@@ -1,4 +0,0 @@
-package com.android.wcf.activity;
-
-public class MilestoneDetailActivity {
-}
--- app/src/main/java/com/android/wcf/fragments/child/ScreenSlidePageFragment.java
+++ app/src/main/java/com/android/wcf/fragments/child/ScreenSlidePageFragment.java
@@ -1,20 +1,20 @@
 package com.android.wcf.fragments.child;
-import android.content.Context;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
 
+import com.android.wcf.R;
 
-public class MilestoneFragment extends Fragment {
+public class ScreenSlidePageFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.fragment_screen_slide_page, container, false);
+                R.layout.fragment_screen_slide, container, false);
 
         return rootView;
     }
