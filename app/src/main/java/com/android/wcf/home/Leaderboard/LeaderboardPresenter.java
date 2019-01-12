6c88a6ab2d0f8c5ef6358bdc58add69c6b4b2197
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-01-12 08:53:55 -0500

    Updated Leaderboard fragment MVP interfaces to retrieve team data. Adding empty methods to be implemented

--- app/src/main/java/com/android/wcf/home/Leaderboard/LeaderboardPresenter.java
+++ app/src/main/java/com/android/wcf/home/Leaderboard/LeaderboardPresenter.java
@@ -1,8 +1,23 @@
 package com.android.wcf.home.Leaderboard;
 
-public class LeaderboardPresenter implements LeaderboardMvp.Presenter {
+import com.android.wcf.R;
+import com.android.wcf.home.BasePresenter;
+import com.android.wcf.model.Team;
+
+import java.util.List;
+
+public class LeaderboardPresenter extends BasePresenter implements LeaderboardMvp.Presenter {
     private static final String TAG = LeaderboardPresenter.class.getSimpleName();
     LeaderboardMvp.LeaderboardView leaderboardView;
+    private List<Team> teams;
+
+    public static final String SORT_COLUMN_NAME = "name";
+    public static final String SORT_COLUMN_AMOUNT = "amount";
+    public static final String SORT_COLUMN_MILES = "miles";
+
+    private String currentSortColumn = SORT_COLUMN_AMOUNT;
+    private boolean currentSortOrderDescending = false;
+
 
     public LeaderboardPresenter(LeaderboardMvp.LeaderboardView view) {
         this.leaderboardView = view;
@@ -11,5 +26,38 @@ public class LeaderboardPresenter implements LeaderboardMvp.Presenter {
     @Override
     public String getTag() {
         return null;
+    }
+
+    @Override
+    protected void onGetTeamListSuccess(List<Team> teams) {
+        super.onGetTeamListSuccess(teams);
+        this.teams = teams;
+        sortTeamsBy(currentSortColumn);
+    }
+
+    @Override
+    protected void onGetTeamListError(Throwable error) {
+        super.onGetTeamListError(error);
+        leaderboardView.showError(R.string.teams_manager_error, error.getMessage());
+    }
+
+    @Override
+    public void toggleSortMode() {
+        currentSortOrderDescending = !currentSortOrderDescending;
+        sortTeamsBy(currentSortColumn);
+
+    }
+
+    @Override
+    public void sortTeamsBy(String sortColumn) {
+        currentSortColumn = sortColumn;
+        if (teams == null || teams.size() == 0) {
+            leaderboardView.showLeaderboardIsEmpty();
+            return;
+        }
+
+        //TODO implement sorting of teams based on the column
+
+        leaderboardView.showLeaderboard(teams);
     }
 }
