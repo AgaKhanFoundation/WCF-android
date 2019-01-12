ba52e2942e876d59e8b9d70d412a2838dd6d94e3
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-01-12 08:55:33 -0500

    Optimize creating of fragments.  Setup to use saved attributes myFbId, myTeamId and myParticipantId

--- app/src/main/java/com/android/wcf/home/HomeActivity.java
+++ app/src/main/java/com/android/wcf/home/HomeActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
@@ -27,32 +26,42 @@ public class HomeActivity extends BaseActivity
     private static final String TAG = HomeActivity.class.getSimpleName();
     private static final String BACK_STACK_ROOT_TAG = "root_fragment";
 
-
     private HomePresenter homePresenter;
     private DashboardFragment dashboardFragment;
     private CampaignFragment campaignFragment;
     private LeaderboardFragment leaderboardFragment;
     private NotificationsFragment notificationsFragment;
 
+    private int currentNaviationId = 0;
+    private int myTeamId = 0;
+    private String myFbId = "skfbid1";
+    private int myParticipantId = 0;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if (currentNaviationId == item.getItemId()) {
+                return false;
+            }
+
             switch (item.getItemId()) {
                 case R.id.nav_dashboard:
-                    loadFragment(dashboardFragment, getString(R.string.nav_dashboard));
+                    loadFragment(dashboardFragment, getString(R.string.nav_dashboard), R.id.nav_dashboard);
                     return true;
                 case R.id.nav_campaign:
-                    loadFragment(campaignFragment, getString(R.string.nav_campaign));
+                    loadFragment(campaignFragment, getString(R.string.nav_campaign), R.id.nav_campaign);
                     return true;
                 case R.id.nav_leaderboard:
-                    loadFragment(leaderboardFragment, getString(R.string.nav_leaderboard));
+                    loadFragment(leaderboardFragment, getString(R.string.nav_leaderboard), R.id.nav_leaderboard);
+                    leaderboardFragment.setMyTeamId(myTeamId);
                     return true;
                 case R.id.nav_notifications:
-                    loadFragment(notificationsFragment, getString(R.string.nav_notifications));
+                    loadFragment(notificationsFragment, getString(R.string.nav_notifications), R.id.nav_notifications);
                     return true;
             }
+
             return false;
         }
     };
@@ -62,11 +71,16 @@ public class HomeActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //TODO: we will initialized these from sharedPreference
+        myTeamId = 0;
+        myFbId = "skfbid1";
+        myParticipantId = 0;
+
         homePresenter = new HomePresenter(this);
 
         dashboardFragment = DashboardFragment.newInstance(null, null);
         campaignFragment = CampaignFragment.newInstance(null, null);
-        leaderboardFragment = LeaderboardFragment.newInstance(null, null);
+        leaderboardFragment = LeaderboardFragment.newInstance(myTeamId);
         notificationsFragment = NotificationsFragment.newInstance(null, null);
 
         BottomNavigationView navigation = findViewById(R.id.home_navigation);
@@ -74,7 +88,7 @@ public class HomeActivity extends BaseActivity
         navigation.setSelectedItemId(R.id.nav_dashboard);
     }
 
-    private void loadFragment(Fragment fragment, String title) {
+    private void loadFragment(Fragment fragment, String title, int navItemId) {
         FragmentManager fm = getSupportFragmentManager();
         // Pop off everything up to and including the current tab
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -86,11 +100,24 @@ public class HomeActivity extends BaseActivity
                 .commit();
 
         setTitle(title);
+        currentNaviationId = navItemId;
     }
 
     private void setTitle(String title) {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(title);
+    }
+
+    public void setMyTeamId(int myTeamId) {
+        this.myTeamId = myTeamId;
+    }
+
+    public void setMyFbId(String myFbId) {
+        this.myFbId = myFbId;
+    }
+
+    public void setMyParticipantId(int myParticipantId) {
+        this.myParticipantId = myParticipantId;
     }
 
     @Override
@@ -111,11 +138,5 @@ public class HomeActivity extends BaseActivity
     @Override
     public void onNotificationFragmentInteraction(Uri uri) {
 
-
     }
 }
-
-
-//        ((MainTabActivity) getActivity()).mTextBack.setVisibility(View.GONE);
-//                ((MainTabActivity) getActivity()).mImageSettings.setVisibility(View.VISIBLE);
-//                ((MainTabActivity) getActivity()).textAppName.setText(getResources().getString(R.string.leaderboard));
