660d1ed408ad126d84fd55f622ae5f3e608005b0
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-02-03 13:48:55 -0500

    Create DTO object for leaderboard team data

---
+++ app/src/main/java/com/android/wcf/model/Constants.kt
@@ -0,0 +1,16 @@
+package com.android.wcf.model
+
+class Constants {
+    companion object {
+
+        const val SORT_MODE_ASCENDING = 0
+        const val SORT_MODE_DESCENDING = 1
+
+        const val TEAM_MAX_SIZE = 10
+        const val STEPS_IN_A_MILE = 2000
+        const val STEPS_PER_UNIT_OF_DONATION = 10000
+        const val AVG_AMOUNT_PER_10K_STEP = 5
+
+
+    }
+}
