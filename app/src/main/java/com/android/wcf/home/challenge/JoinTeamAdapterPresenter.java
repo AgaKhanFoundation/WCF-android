0b6c3c156f17a281d86820293e841788c2617400
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-08-17 15:52:51 -0400

    Join a Team - view updated per revised design

---
+++ app/src/main/java/com/android/wcf/home/challenge/JoinTeamAdapterPresenter.java
@@ -0,0 +1,69 @@
+package com.android.wcf.home.challenge;
+
+import androidx.annotation.NonNull;
+
+import com.android.wcf.model.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JoinTeamAdapterPresenter implements JoinTeamAdapterMvp.Presenter {
+
+    JoinTeamAdapterMvp.View view;
+    List<Team> teams = new ArrayList<>();
+    JoinTeamAdapterMvp.Host host;
+
+    int selectedTeamPosition = -1;
+
+    public JoinTeamAdapterPresenter(@NonNull JoinTeamAdapterMvp.View view, @NonNull JoinTeamAdapterMvp.Host host) {
+        this.view = view;
+        this.host = host;
+    }
+
+    @Override
+    public void updateTeamsData(List<Team> teams) {
+        this.teams.clear();
+        this.teams.addAll(teams);
+        view.teamsDataUpdated();
+    }
+
+    @Override
+    public Team getTeam(int pos) {
+        if (teams != null && pos < teams.size() && pos >= 0) {
+            return teams.get(pos);
+        }
+        return null;
+    }
+
+    @Override
+    public int getTeamsCount() {
+        return teams != null ? teams.size() : 0;
+    }
+
+    @Override
+    public void onTeamSelected(int pos) {
+        selectedTeamPosition = pos;
+        view.teamRowSelected(pos);
+    }
+
+    @Override
+    public Team getSelectedTeam() {
+        return selectedTeamPosition != -1 ? getTeam(selectedTeamPosition) : null;
+    }
+
+    @Override
+    public int getSelectedTeamPosition() {
+        return selectedTeamPosition;
+    }
+
+    @Override
+    public void clearTeamSelectionPosition() {
+        selectedTeamPosition = -1;
+    }
+
+    @Override
+    public void teamRowSelected(int pos) {
+        host.teamRowSelected(pos);
+
+    }
+}
