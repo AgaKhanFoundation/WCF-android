1880de430a372cf7e79de62a5399480daad93869
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-10-04 23:00:02 -0400

    Date helper methods

---
+++ app/src/main/java/com/android/wcf/helper/DateTimeHelper.kt
@@ -0,0 +1,162 @@
+package com.android.wcf.helper
+
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.absoluteValue
+
+/**
+ * Created by jwcona0510 on 6/16/17.
+ */
+
+const val ISO_1860_W_TIME_ZONE = "yyyy-MM-dd'T'HH:mm:ssZZZ"
+const val MM_DD_HH_MM_SS_MMM = "MM-dd HH:mm:ss.SSS"
+
+
+const val MILLIS_IN_HOUR: Int = 1000 * 60 * 60
+const val MILLIS_IN_MINUTE: Int = 1000 * 60
+const val MILLIS_IN_SECOND: Int = 1000
+
+const val HOUR_INDX = 0
+const val MIN_INDX = 1
+const val SEC_INDX = 2
+const val MILLI_INDX = 3
+
+class DateTimeHelper {
+
+    companion object {
+        fun getNowLong(): Long {
+            return System.currentTimeMillis()
+        }
+
+        fun intToUserReadableTime(i: Int): String {
+            val times = generateTimeUnitsArray(i.toLong())
+            return generateUiTimeString(times)
+        }
+
+        fun intToReadableTime(i: Int): String {
+            val times = generateTimeUnitsArray(i.toLong())
+            return generateTimeString(times)
+        }
+
+        fun longToReadableTime(l: Long): String {
+            val times = generateTimeUnitsArray(l)
+            return generateTimeString(times)
+        }
+
+        private fun generateTimeUnitsArray(l: Long): LongArray {
+            val timeUnits = LongArray(4)
+            timeUnits[HOUR_INDX] = l / MILLIS_IN_HOUR
+            timeUnits[MIN_INDX] = (l % MILLIS_IN_HOUR) / MILLIS_IN_MINUTE
+            timeUnits[SEC_INDX] = (l % MILLIS_IN_MINUTE) / MILLIS_IN_SECOND
+            timeUnits[MILLI_INDX] = l % MILLIS_IN_SECOND
+
+            return timeUnits
+        }
+
+        private fun generateTimeString(timeUnits: LongArray): String {
+            val hours = timeUnits[HOUR_INDX].absoluteValue
+            val minutes = timeUnits[MIN_INDX].absoluteValue
+            val seconds = timeUnits[SEC_INDX].absoluteValue
+            var millis = timeUnits[MILLI_INDX]
+
+            val sb = StringBuilder(if (millis < 0L) {
+                millis = millis.absoluteValue; "-"
+            } else "")
+            sb.append(if (hours < 10) "0" + hours.toString() else hours.toString())
+                    .append(":")
+                    .append(if (minutes < 10) "0" + minutes.toString() else minutes.toString())
+                    .append(":")
+                    .append(if (seconds < 10) "0" + seconds.toString() else seconds.toString())
+                    .append(".")
+                    .append(when {
+                        (millis < 10) -> "00" + millis.toString()
+                        (millis < 100) -> "0" + millis.toString()
+                        else -> millis.toString()
+                    })
+
+            return sb.toString()
+        }
+
+        private fun generateUiTimeString(timeUnits: LongArray): String {
+            val hours = timeUnits[HOUR_INDX]
+            val minutes = timeUnits[MIN_INDX]
+            val seconds = timeUnits[SEC_INDX]
+
+            val sb = StringBuilder("")
+            val hasHours = hours > 0
+            val hasMinutes = minutes > 0
+            val hasSeconds = seconds >= 0
+            if (hasHours) {
+                sb.append(hours.toString())
+                        .append(":")
+            }
+            if (hasMinutes) {
+                sb.append(if (minutes in 1..9) "0" + minutes.toString() else minutes.toString())
+                        .append(":")
+            } else {
+                sb.append("00").append(":")
+            }
+
+            sb.append(if (seconds < 10) "0" + seconds.toString() else seconds.toString())
+
+            return sb.toString()
+        }
+
+        fun obtainDateForNow(): String = longToReadableDate(System.currentTimeMillis())
+
+        fun getDateForNow(): Date {
+            val cal: Calendar = Calendar.getInstance()
+            cal.timeInMillis = System.currentTimeMillis()
+            return cal.time
+        }
+
+        fun getDateForYesterday(): Date {
+            val cal: Calendar = Calendar.getInstance()
+            cal.add(Calendar.DATE, -1);
+            cal.timeInMillis = System.currentTimeMillis()
+            return cal.time
+        }
+
+        private fun longToReadableDate(l: Long): String {
+            val cal: Calendar = Calendar.getInstance()
+            cal.timeInMillis = l
+            val date: Date = cal.time
+            val dateForm: DateFormat = SimpleDateFormat.getDateTimeInstance()
+            return dateForm.format(date)
+        }
+
+        fun obtainISO8601DateForNow(): String = longToISO8601(System.currentTimeMillis())
+
+        fun obtainobtainISO8601Date(l: Long) = longToISO8601(l)
+
+        private fun longToISO8601(l: Long): String {
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = l
+            val date: Date = cal.time
+            val dateFormat = SimpleDateFormat(ISO_1860_W_TIME_ZONE)
+            return dateFormat.format(date)
+        }
+
+        fun obtainDebuggingDate(l: Long): String {
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = l
+            val date: Date = cal.time
+            val dateFormat = SimpleDateFormat(MM_DD_HH_MM_SS_MMM)
+            return dateFormat.format(date)
+        }
+
+        fun obtainDebuggingDateForNow() = obtainDebuggingDate(System.currentTimeMillis())
+
+
+        fun calcTimeDiffFromNow(eventTime: Long): Long {
+            return System.currentTimeMillis() - eventTime
+        }
+
+        fun getUtcOffetForlocal():Int {
+            return TimeZone.getDefault().getOffset(System.currentTimeMillis())
+        }
+
+    }
+}
