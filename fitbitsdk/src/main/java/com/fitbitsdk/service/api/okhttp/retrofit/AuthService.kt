923a58a7b5d1522b456c5a08424b45766bc7f881
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-10-10 23:26:25 -0400

    Fitbit refresh_token api called fixed

--- fitbitsdk/src/main/java/com/fitbitsdk/service/api/okhttp/retrofit/AuthService.kt
+++ fitbitsdk/src/main/java/com/fitbitsdk/service/api/okhttp/retrofit/AuthService.kt
@@ -5,19 +5,24 @@ import com.fitbitsdk.service.models.auth.OAuthAccessToken
 import com.fitbitsdk.service.models.auth.RequestRefreshTokenModel
 import com.fitbitsdk.service.models.auth.RequestTokenModel
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 interface AuthService {
     companion object {
-        const val TOKEN_ISSUING_ENDPOINT: String = "issue/oauth2/token"
+        const val TOKEN_ISSUING_ENDPOINT: String = "oauth2/token"
     }
 
     @POST(TOKEN_ISSUING_ENDPOINT)
     fun authorizeToken(@Header(AuthEndpoint.OAUTH_HEADER_KEY) basicAuth: String, @Body tokenModel : RequestTokenModel): Call<OAuthAccessToken>
 
+//    @POST(TOKEN_ISSUING_ENDPOINT)
+//    fun refreshToken(@Header(AuthEndpoint.OAUTH_HEADER_KEY) basicAuth: String, @Body refreshTokenModel : RequestRefreshTokenModel): Call<OAuthAccessToken>
+
+    @FormUrlEncoded
     @POST(TOKEN_ISSUING_ENDPOINT)
-    fun refreshToken(@Header(AuthEndpoint.OAUTH_HEADER_KEY) basicAuth: String, @Body refreshTokenModel : RequestRefreshTokenModel): Call<OAuthAccessToken>
+    fun refreshToken(@Header(AuthEndpoint.OAUTH_HEADER_KEY) basicAuth: String,
+                      @Field("grant_type") grantType: String,
+                      @Field("refresh_token") refresh_token: String): Call<OAuthAccessToken>
+
 }
