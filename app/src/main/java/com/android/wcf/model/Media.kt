c99311e0b9a6d02cc7e2ff4311d02689a464d323
Sultan Keshwani <sultan.keshwani@gmail.com>
2019-11-02 21:59:56 -0400

    Add subtext and journey_map attributes. Add getMediaContent() method to parse the media and create the list for ViewPager

--- app/src/main/java/com/android/wcf/model/Media.kt
+++ app/src/main/java/com/android/wcf/model/Media.kt
@@ -2,9 +2,14 @@ package com.android.wcf.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Media(
-        @SerializedName("id") var id: Int = 0,
-        @SerializedName("type") var type: String? = "",
-        @SerializedName("url") var url: String? = ""){
+
+enum class MediaType {
+    PHOTO, VIDEO
 }
 
+data class Media(
+        @SerializedName("id") var id: Int = 0,
+        @SerializedName("sequence") var sequence: Int = 0,
+        @SerializedName("type") var type: MediaType,
+        @SerializedName("url") var url: String? = null){
+}
--- app/src/main/java/com/android/wcf/model/Milestone.kt
+++ app/src/main/java/com/android/wcf/model/Milestone.kt
@@ -1,21 +1,49 @@
 package com.android.wcf.model
 
+import android.util.Log
 import com.google.gson.annotations.SerializedName
 
 data class Milestone(
         @SerializedName("id") var id: Int = 0,
         @SerializedName("sequence") var sequence: Int = 0,
-        @SerializedName("distance") var steps: Int = 9999999,
+        @SerializedName("distance") var steps: Int = 999999999,
         @SerializedName("name") var name: String = "",
+        @SerializedName("subtext") var subtext: String = "",
         @SerializedName("flag_name") var flag_name: String = "",
-        @SerializedName("title") var title: String? = null,
-        @SerializedName("subtitle") var subtitle: String? = null,
-        @SerializedName("description") var description: String? = "",
-        @SerializedName("media") var media: String? = null) {
+        @SerializedName("journey_map") var journey_map: String = "",
+        @SerializedName("title") var title: String = "",
+        @SerializedName("subtitle") var subtitle: String = "",
+        @SerializedName("description") var description: String = "",
+        @SerializedName("media") var media: String = "") {
 
     var reached:Boolean = false
 
     fun hasReached(stepsCompleted:Int) {
         reached = stepsCompleted >= steps
+    }
+
+    fun getMediaContent():List<Media> {
+        val mediaList:MutableList<Media> = mutableListOf()
+        val parsedMedia:List<String> = media.split(" ".toRegex(), 2)
+        var seq = 0
+        for (item in parsedMedia) {
+            val parsedItem:List<String> = item.split(":".toRegex(), 2)
+            if (parsedItem.size == 2) {
+                val mediaType:MediaType =
+                        if (parsedItem.get(0).toLowerCase().equals("video")) MediaType.VIDEO
+                            else MediaType.PHOTO
+
+                val media = Media(0, seq++, mediaType, parsedItem.get(1)  )
+                mediaList.add(media)
+            }
+            else {
+                Log.e(TAG, "media ignored - incorrect format for media item: $item")
+            }
+        }
+        return mediaList
+    }
+
+    companion object {
+        val TAG = Milestone::class.java.simpleName
     }
 }
